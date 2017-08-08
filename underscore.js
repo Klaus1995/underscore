13aebe13439a2157be4ab86476367ab28aa19012
Klaus <839887585@qq.com>
2017-08-08 17:47:24 +0800

    update

--- underscore.js
+++ underscore.js
@@ -21,6 +21,9 @@
 		isObject(object) {
 			return Object.prototype.toString.call(object) === '[object Object]';
 		},
+		isFunction(object) {
+			return Object.prototype.toString.call(object) === '[object Function]';
+		},
 		each(list, iteratee, context) {
 			if (_.isArray(list)) {
 				list.forEach(iteratee, context);
@@ -50,13 +53,9 @@
 			let result;
 			if (_.isArray(list)) {
 				if (memo) {
-					result = list.reduce(function(sum, value, index, array) {
-						return iteratee.call(context, sum, value, index, array);
-					}, memo);
+					result = list.reduce((sum, value, index, array) => iteratee.call(context, sum, value, index, array), memo);
 				} else {
-					result = list.reduce(function(sum, value, index, array) {
-						return iteratee.call(context, sum, value, index, array);
-					});
+					result = list.reduce((sum, value, index, array) => iteratee.call(context, sum, value, index, array));
 				}
 			} else if (_.isNumber(list.length)) {
 				result = _.reduce(Array.from(list), iteratee, memo, context);
@@ -77,13 +76,9 @@
 			let result;
 			if (_.isArray(list)) {
 				if (memo) {
-					result = list.reduceRight(function(sum, value, index, array) {
-						return iteratee.call(context, sum, value, index, array);
-					}, memo);
+					result = list.reduceRight((sum, value, index, array) => iteratee.call(context, sum, value, index, array), memo);
 				} else {
-					result = list.reduceRight(function(sum, value, index, array) {
-						return iteratee.call(context, sum, value, index, array);
-					});
+					result = list.reduceRight((sum, value, index, array) => iteratee.call(context, sum, value, index, array));
 				}
 			} else if (_.isNumber(list.length)) {
 				result = _.reduceRight(Array.from(list), iteratee, memo, context);
@@ -139,9 +134,7 @@
 			return result;
 
 			function check(obj) {
-				return entries.every(function(item) {
-					return obj[item[0]] === item[1];
-				});
+				return entries.every((item) => obj[item[0]] === item[1]);
 			}
 		},
 		findWhere(list, properties) {
@@ -151,10 +144,125 @@
 			return result;
 
 			function check(obj) {
-				return entries.every(function(item) {
-					return obj[item[0]] === item[1];
-				});
+				return entries.every((item) => obj[item[0]] === item[1]);
 			}
+		},
+		reject(list, predicate, context) {
+			let result = [];
+			if (_.isArray(lsit)) {
+				list.forEach((item, index, array) => {
+					if (!predicate.call(context, item, index, array)) {
+						result.push(item);
+					}
+				});
+			} else if (_.isNumber(list.length)) {
+				result = _.reject(Array.from(list), predicate, context);
+			} else if (_.isObject(list)) {
+				for (let key of Object.keys(list)) {
+					if (!predicate.call(context, list[key], key, list)) {
+						result.push(list[key]);
+					}
+				}
+			}
+			return result;
+		},
+		every(list, predicate = (value) => !!value, context) {
+			let result = true;
+			if (_.isArray(list)) {
+				result = list.every(predicate, context);
+			} else if (_.isNumber(list.length)) {
+				result = _.every(Array.from(list), predicate, context);
+			} else if (_.isObject(list)) {
+				for (let key of Object.keys(list)) {
+					if (!predicate.call(context, list[key], key, list)) {
+						result = false;
+						break;
+					}
+				}
+			}
+			return result;
+		},
+		some(list, predicate = (value) => !!value, context) {
+			let result = false;
+			if (_.isArray(list)) {
+				result = list.some(predicate, context);
+			} else if (_.isNumber(list.length)) {
+				result = _.some(Array.from(list), predicate, context);
+			} else if (_.isObject(list)) {
+				for (let key of Object.keys(list)) {
+					if (predicate.call(context, list[key], key, list)) {
+						result = true;
+						break;
+					}
+				}
+			}
+			return result;
+		},
+		contains(list, value) {
+			let result = false;
+			if (_.isArray(list)) {
+				result = list.indexOf(value) !== -1;
+			} else if (_.isNumber(list)) {
+				result = _.contains(Array.from(list), value);
+			} else if (_.isObject(list)) {
+				for (let listValue of Object.values(list)) {
+					if (listValue === value) {
+						result = true;
+						break;
+					}
+				}
+			}
+			return result;
+		},
+		invoke(list, methodName, ...args) {
+			let result = [];
+			result = _.map(list, (item) => item[methodName].call(item, ...args));
+			return result;
+		},
+		pluck(list, propertyName) {
+			let result = [];
+			result = _.map(list, (item) => item[propertyName]);
+			return result;
+		},
+		max(list, iteratee = (item) => item, context) {
+			let result;
+			result = _.reduce(list, function(memo, value, index, array) {
+				if (iteratee.call(context, memo, index, array) >= iteratee.call(context, value, index, array)) {
+					return memo;
+				} else {
+					return value;
+				}
+			})
+			return result;
+		},
+		min(list, iteratee = (item) => item, context) {
+			let result;
+			result = _.reduce(list, function(memo, value, index, array) {
+				if (iteratee.call(context, memo, index, array) <= iteratee.call(context, value, index, array)) {
+					return memo;
+				} else {
+					return value;
+				}
+			})
+			return result;
+		},
+		sortBy(list, iteratee, context) {
+			let result = [];
+			iteratee = _.isFunction(iteratee) ? iteratee : (item) => item[iteratee];
+			result = _.map(list, function(item, index, list) {
+				return {
+					item: item,
+					value: iteratee.call(context, item, index, list)
+				};
+			});
+			result.sort(function(a, b) {
+				return a.value <= b.value ? -1 : 1;
+			});
+			result = _.pluck(result, 'item');
+			return result;
+		},
+		groupBy(list, iteratee, context) {
+
 		}
 	};
 	window._ = _;
