9d8804872bddca30bb8cdc2dda65126da45a6942
Klaus <839887585@qq.com>
2017-08-11 18:15:22 +0800

    update

--- new.js
+++ new.js
@@ -43,9 +43,270 @@
 			return _.matcher(value);
 		}
 
-		//如果传入剩下的类型值（null，number，boolean，string），则返回_.property返回对象的某一特定属性的值
+		//如果传入剩下的类型值（null，number，boolean，string, synbol），则返回_.property返回对象的某一特定属性的值
 		return _.property(value);
 	}
+
+
+	//集合函数 (Collections Functions)
+
+
+	//each方法
+	//_.each(list, iteratee, [context])
+	//遍历list中的所有元素，按顺序用遍历输出每个元素。如果传递了context参数，则把iteratee绑定到context对象上。每次调用iteratee都会传递三个参数：(element, index, list)。如果list是个JavaScript对象，iteratee的参数是 (value, key, list))。返回list以方便链式调用。
+	_.each = function(list, iteratee, context) {
+
+		iteratee = createCallback(iteratee, context);
+
+		if (_.isNumber(list.length)) {
+			list = Array.from(list);
+			list.forEach(iteratee);
+		} else {
+			for (let [key, value] of Object.entries(list)) {
+				iteratee(value, key, list);
+			}
+		}
+
+		return list;
+	};
+
+	//map方法
+	//_.map(list, iteratee, [context]) 
+	//通过转换函数(iteratee迭代器)映射列表中的每个值产生价值的新数组。iteratee传递三个参数：value，然后是迭代 index(或 key 愚人码头注：如果list是个JavaScript对象是，这个参数就是key)，最后一个是引用指向整个list。
+	_.map = function(list, iteratee, context) {
+
+		let result = [];
+
+		iteratee = createCallback(iteratee, context);
+
+		if (_.isNumber(list.length)) {
+			list = Array.from(list);
+			result = list.map(iteratee);
+		} else {
+			for (let [key, value] of Object.entries(list)) {
+				result.push(iteratee(value, key, list));
+			}
+		}
+
+		return result;
+	};
+
+	//reduce方法
+	//_.reduce(list, iteratee, [memo], [context])
+	//别名为 inject 和 foldl, reduce方法把list中元素归结为一个单独的数值。Memo是reduce函数的初始值，reduce的每一步都需要由iteratee返回。这个迭代传递4个参数：memo,value 和 迭代的index（或者 key）和最后一个引用的整个 list。
+	//如果没有memo传递给reduce的初始调用，iteratee不会被列表中的第一个元素调用。第一个元素将取代 传递给列表中下一个元素调用iteratee的memo参数。
+	_.reduce = function(list, iteratee, memo, context) {
+
+		let result;
+
+		iteratee = createCallback(iteratee, context);
+
+		if (_.isNumber(list.length)) {
+			list = Array.from(list);
+			if (_.isUndefined(memo)) {
+				result = list.reduce(iteratee);
+			} else {
+				result = list.reduce(iteratee, memo);
+			}
+		} else {
+			let entries = Object.entries(list);
+			if (_.isUndefined(memo)) {
+				memo = entries.shift()[1];
+			}
+			for (let [key, value] of entries) {
+				memo = iteratee(memo, value, key, list);
+			}
+			result = memo;
+		}
+
+		return result;
+	};
+
+	//reduceRight方法
+	//_.reduceRight(list, iteratee, memo, [context]) 
+	//reducRight是从右侧开始组合的元素的reduce函数，如果存在JavaScript 1.8版本的reduceRight，则用其代替。
+	_.reduceRight = function(list, iteratee, memo, context) {
+
+		let result;
+
+		iteratee = createCallback(iteratee, context);
+
+		if (_.isNumber(list.length)) {
+			list = Array.from(list);
+			if (_.isUndefined(memo)) {
+				result = list.reduceRight(iteratee);
+			} else {
+				result = list.reduceRight(iteratee, memo);
+			}
+		} else {
+			let entries = Object.entries(list).reverse();
+			if (_.isUndefined(memo)) {
+				memo = entries.shift()[1];
+			}
+			for (let [key, value] of entries) {
+				memo = iteratee(memo, value, key, list);
+			}
+			result = memo;
+		}
+
+		return result;
+	};
+
+	//find方法
+	//_.find(list, predicate, [context]) 
+	//在list中逐项查找，返回第一个通过predicate迭代函数真值检测的元素值，如果没有值传递给测试迭代器将返回undefined。 如果找到匹配的元素，函数将立即返回，不会遍历整个list。
+	_.find = function(list, predicate, context) {
+
+		let result;
+
+		predicate = createCallback(predicate, context);
+
+		if (_.isNumber(list.length)) {
+			list = Array.from(list);
+			result = list.find(predicate);
+		} else {
+			for (let [key, value] of Object.entries(list)) {
+				if (predicate(value, key, list)) {
+					result = value;
+					break;
+				}
+			}
+		}
+
+		return result;
+	};
+
+	//filter方法
+	//_.filter(list, predicate, [context]) 
+	//遍历list中的每个值，返回包含所有通过predicate真值检测的元素值。（愚人码头注：如果存在原生filter方法，则用原生的filter方法。）
+	_.filter = function(list, predicate, context) {
+
+		let result = [];
+
+		predicate = createCallback(predicate, context);
+
+		_.each(list, function(value, key, list) {
+
+			if (predicate(value, key, list)) {
+				result.push(value);
+			}
+		});
+
+		return result;
+	};
+
+	//where方法
+	//_.where(list, properties) 
+	//遍历list中的每一个值，返回一个数组，这个数组包含properties所列出的属性的所有的 键 - 值对。
+	_.where = function(list, properties) {
+
+		return _.filter(list, _.matcher(properties));
+	};
+
+	//findWhere方法
+	//_.findWhere(list, properties) 
+	//遍历整个list，返回匹配 properties参数所列出的所有 键 - 值 对的第一个值。
+	//如果没有找到匹配的属性，或者list是空的，那么将返回undefined。
+	_.findWhere = function(list, properties) {
+
+		return _.find(list, _.matcher(properties));
+	};
+
+
+	//数组函数（Array Functions）
+
+
+	//first方法
+	//_.first(array, [n])
+	//返回array（数组）的第一个元素。传递 n参数将返回数组中从第一个元素开始的n个元素（愚人码头注：返回数组中前 n 个元素.）。
+	_.first = function(array, n) {
+
+		array = Array.from(array);
+
+		if (!_.isUndefined(n)) {
+			if (n > array.length) {
+				n = array.length;
+			} else if (n < 0) {
+				n = 0;
+			}
+			return array.slice(0, n);
+		}
+
+		return array[0];
+	};
+
+	//initial方法
+	//_.initial(array, [n]) 
+	//返回数组中除了最后一个元素外的其他全部元素。 在arguments对象上特别有用。传递 n参数将从结果中排除从最后一个开始的n个元素（愚人码头注：排除数组后面的 n 个元素）。
+	_.initial = function(array, n = 1) {
+
+		array = Array.from(array);
+
+		if (n > array.length) {
+			n = array.length;
+		} else if (n < 0) {
+			n = 0;
+		}
+
+		return array.slice(0, array.length - n);
+	};
+
+	//last方法
+	//_.last(array, [n]) 
+	//返回array（数组）的最后一个元素。传递 n参数将返回数组中从最后一个元素开始的n个元素（愚人码头注：返回数组里的后面的n个元素）。
+	_.last = function(array, n) {
+
+		array = Array.from(array);
+
+		if (!_.isUndefined(n)) {
+			if (n > array.length) {
+				n = array.length;
+			} else if (n < 0) {
+				n = 0;
+			}
+			return array.slice(array.length - n, array.length);
+		}
+
+		return array[array.length - 1];
+	};
+
+	//rest方法
+	//_.rest(array, [index])
+	//返回数组中除了第一个元素外的其他全部元素。传递 index 参数将返回从index开始的剩余所有元素 。
+	_.rest = function(array, index = 1) {
+
+		array = Array.from(array);
+
+		return array.slice(index, array.length);
+	};
+
+	//compact方法
+	//_.compact(array) 
+	//返回一个除去所有false值的 array副本。 在javascript中, false, null, 0, "", undefined 和 NaN 都是false值.
+	_.compact = function(array) {
+
+		return _.filter(array, _.identity);
+	};
+
+	//flatten方法
+	//_.flatten(array, [shallow]) 
+	//将一个嵌套多层的数组 array（数组） (嵌套可以是任何层数)转换为只有一层的数组。 如果你传递 shallow参数，数组将只减少一维的嵌套。
+	_.flatten = function(array, shallow) {
+
+		let result = [];
+
+		flatten(array, shallow);
+
+		return result;
+
+		function flatten(array, shallow) {
+
+			array = Array.from(array);
+
+
+		}
+	};
+
 
 	//对象函数（Object Functions）
 
@@ -187,11 +448,15 @@
 					result[key] = object[key];
 				}
 			}
-		} else {
-			for (let key of args) {
-				if (key in object) {
-					result[key] = object[key];
-				}
+		}
+
+		if (_.isArray(args[0])) {
+			args = [...args[0]];
+		}
+
+		for (let key of args) {
+			if (key in object) {
+				result[key] = object[key];
 			}
 		}
 
@@ -213,11 +478,15 @@
 					result[key] = object[key];
 				}
 			}
-		} else {
-			for (let key in object) {
-				if (args.indexOf(key) === -1) {
-					result[key] = object[key];
-				}
+		}
+
+		if (_.isArray(args[0])) {
+			args = [...args[0]];
+		}
+
+		for (let key in object) {
+			if (args.indexOf(key) === -1) {
+				result[key] = object[key];
 			}
 		}
 
@@ -243,7 +512,7 @@
 	//clone方法
 	//_.clone(object) 
 	//创建 一个浅复制（浅拷贝）的克隆object。任何嵌套的对象或数组都通过引用拷贝，不会复制。
-	_.clone = function(objcet) {
+	_.clone = function(object) {
 
 		if (_.isArray(object)) {
 			return object.slice();
@@ -416,8 +685,8 @@
 
 			//只剩下狭义上的object
 			//首先判断对象的构造函数，如果不同则认为是unequal
-			let aCtor = a.constructor,
-				bCtor = b.constructor;
+			let aCtor = a.__proto__,
+				bCtor = b.__proto__;
 
 			//第二个判断条件是为了不同iframe间内置的Object构造函数不同，
 			if (aCtor !== bCtor && !(_.isFunction(aCtor) && aCtor instanceof aCtor && _.isFunction(bCtor) && bCtor instanceof bCtor)) {
@@ -445,29 +714,91 @@
 		}
 	};
 
-	//isObject方法  
-	//function和object会返回true，null会返回false
-	_.isObject = function(obj) {
-		const type = typeof obj;
-		//除去null类型
-		if (!obj) return false;
-		return type === 'function' || type === 'object';
+	//isMatch方法
+	//_.isMatch(object, properties) 
+	//告诉你properties中的键和值是否包含在object中。
+	_.isMatch = function(object, properties) {
+
+		for (let [key, value] of Object.entries(properties)) {
+			if (object[key] !== value || !(key in object)) {
+				return false;
+			}
+		}
+
+		return true;
+	};
+
+	//isEmpty方法
+	//_.isEmpty(object) 
+	//如果object 不包含任何值(没有可枚举的属性)，返回true。 对于字符串和类数组（array-like）对象，如果length属性为0，那么_.isEmpty检查返回true。
+	_.isEmpty = function(object) {
+
+		if (_.isNull(object)) {
+			return true;
+		}
+
+		if (_.isArray(object) || _.isString(object)) {
+			return object.length === 0;
+		}
+
+		if (_.isObject(object)) {
+			if (_.isNumber(object.length)) {
+				return object.length === 0;
+			}
+
+			return Object.keys(object).length === 0;
+		}
+	};
+
+	//isElement方法
+	//_.isElement(object) 
+	//如果object是一个DOM元素，返回true。
+	_.isElement = function(object) {
+
+		return object.nodeType === 1;
 	};
 
 	//isArray方法
+	//_.isArray(object)
+	//如果object是一个数组，返回true。
 	//因为有内置的方法所以单独提出来
 	_.isArray = Array.isArray;
+
+	//isObject方法  
+	//_.isObject(value) 
+	//如果object是一个对象，返回true。需要注意的是JavaScript数组和函数是对象，字符串和数字不是。
+	//function和object会返回true，null会返回false
+	_.isObject = function(object) {
+
+		const type = typeof object;
+
+		//除去null类型
+		if (!object) return false;
+
+		return type === 'function' || type === 'object';
+	};
 
 	//isUndefined,isNull,isString,isNumber,isBoolean,isFunction,isDate,isRegExp,isArguments,isSet,isMap,isWeakeSet,isWeakMap,isSymbol方法
 	//因为方法类似，所以统一定义
 	//数据类型数组集合
-	const nativeTypes = ['Undefined', 'Null', 'String', 'Number', 'Boolean', 'Function', 'Date', 'RegExp', 'Arguments', 'Set', 'Map', 'WeakSet', 'WeakMap', 'Symbol'];
-	nativeTypes.forEach((type) => {
-		_['is' + type] = function(obj) {
-			return Object.prototype.toString.call(obj) === '[object ' + type + ']';
+	['Undefined', 'Null', 'String', 'Number', 'Boolean', 'Function', 'Date', 'RegExp', 'Arguments', 'Error', 'Set', 'Map', 'WeakSet', 'WeakMap', 'Symbol'].forEach(function(type) {
+
+		_['is' + type] = function(object) {
+
+			return Object.prototype.toString.call(object) === '[object ' + type + ']';
 		};
 	});
 
+	//isFinite方法
+	//_.isFinite(object) 
+	//如果object是一个有限的数字，返回true。
+	_.isFinite = Number.isFinite;
+
+	//isNaN方法
+	//_.isNaN(object) 
+	//如果object是 NaN，返回true。 
+	//注意： 这和原生的isNaN 函数不一样，如果变量是undefined，原生的isNaN 函数也会返回 true 。
+	_.isNaN = Number.isNaN;
 
 	_.identity = function(value) {
 		return value;
